9e19c343fbbd0739fba67f7cbb5a908ec521a831
uwagbaesson <auwaldoma@yahoo.com>
2016-05-30 23:19:29 +0200

    the 3rd time

--- hellogit/src/hellogit/Run.java
+++ hellogit/src/hellogit/Run.java
@@ -2,9 +2,12 @@ package hellogit;
 
 public class Run {
 	public static void main(String[] args) {
+		//changes before commit2
+		
+		
 		System.out.println("Hello");
 		
-		//changes before commit2
+		
 	}
 
 }
